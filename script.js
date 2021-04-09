c0bda05bfd9b23491af4824c871057dba3944e02
El-Glory <elgloryoriabure@gmail.com>
2021-04-09 14:58:38 +0100

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,80 @@
-function history(){
-	return document.getElementById("historyValue").innerText
+function getHistory() {
+	return document.getElementById("history-value").innerText;
 }
-
+//prints numbers to the history section
 function printHistory(num){
-	document.getElementById("historyValue").innerText= num
+	return document.getElementById("history-value").innerText = num;
 }
 
-function output(){
-	return document.getElementById("outputValue").innerText
+function getOutput() {
+	return document.getElementById("output-value").innerText;
 }
 
 function printOutput(num){
-	document.getElementById("outputValue").innerText= num
+	if (num === "") {
+		document.getElementById("output-value").innerText = num;
+	}else{
+		return document.getElementById("output-value").innerText = FormattedNumber(num);
+	}
+}
+//Appropraites dot to is rightful place
+function FormattedNumber(num){
+	if (num == '-') {
+		return "";
+	}
+	let n = Number(num);
+	let value = n.toLocaleString('en');
+	return value;
+}
+//reverse number to its rightful state
+function reverseNumberFormat(num){
+	return Number(num.replace(/,/g,''))
 }
 
-printOutput("99999")
+ let operator = document.getElementsByClassName("operator");
+
+ for (var i = 0; i < operator.length; i++) {
+ 	operator[i].addEventListener('click', function(){
+ 		if (this.id =="clear") {
+ 			printHistory('');
+ 			printOutput('');	
+ 		}
+
+ 		else if (this.id == 'backspace') {
+ 			var output = reverseNumberFormat(getOutput()).toString();
+ 			if (output) {
+ 				output= output.substr(0,output.length-1);
+ 				printOutput(output);
+ 			}
+ 		}else{
+ 			var output = getOutput();
+ 			var history = getHistory();
+ 			if (output != '') {
+ 				output = reverseNumberFormat(output);
+ 				history=history+output;
+ 				if (this.id == "=") {
+ 					var result = eval(history);
+ 					printOutput(result);
+ 					printHistory('');
+ 				}else{
+ 					history = history +this.id;
+ 					printHistory(history);
+ 					printOutput('');
+ 				}
+ 			}
+ 		}
+ 	});
+ }
+
+ let number = document.getElementsByClassName("number");
+
+ for (var i = 0; i < number.length; i++) {
+ 	number[i].addEventListener('click', function(){
+ 		var output = reverseNumberFormat(getOutput());
+ 		if (output!=NaN) {
+ 			output =output+this.id;
+ 			printOutput(output);
+ 		}
+ 	});
+ }
+
